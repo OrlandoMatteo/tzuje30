d54225354a30d726779f6e59ebfc3866e48787ec
Matteo Orlando <matteo.orlando.1993@gmail.com>
2023-02-22 17:10:01 +0100

    First commit tzuje

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{astro,html,jsx,tsx,svelte,vue,js,ts}"],
+	content: ["./src/**/*.{astro,html,jsx,tsx,svelte,vue,js,ts}",
+		"./node_modules/flowbite/**/*.js"],
 	theme: {
 		extend: {
 			screens: {
 				sm: "400px",
 			},
+			colors: {
+				"tzuje": '#4ade80'
+			}
 		},
 	},
-	plugins: [],
+	plugins: [
+	],
+	corePlugins: {
+	}
 };
